eae83f955f4c64efd7a0a50da0960e089ddc84ba
yaser <ddemety372@gmail.com>
2022-05-25 23:58:40 +0300

    ()

--- duplicate/app.js
+++ duplicate/app.js
@@ -1,17 +1,34 @@
 // Write a JS code to find the duplicate number in an array.
 
+// let arr1 = [4, 2, 34, 4, 1, 12, 1, 4,12];
+// let result = [];
 
-let arr1 = [4, 2, 34, 4, 1, 12, 1, 4,12];
+// function findDuplicate(arr) {
+//   for (let i = arr.length; i >= 0; i--) {
+//     let deleted = arr.pop();
+
+//     if (arr.includes(deleted) && !result.includes(deleted)) {
+//       result.push(deleted);
+//     }
+//   }
+//   return result;
+// }
+// console.log(findDuplicate(arr1));
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+
+let str1 = "recede";
+str1 = str1.toLowerCase();
 let result = [];
-
-function findDuplicate(arr) {
-  for (let i = arr.length; i >= 0; i--) {
-    let deleted = arr.pop();
-    
-    if (arr.includes(deleted) && !result.includes(deleted)) {
-      result.push(deleted);
-    }
+str1.split("").forEach(function (char) {
+  if (str1.split(char).length > 2) {
+    result.push(")");
+  } else {
+    result.push("(");
   }
-  return result;
-}
-console.log(findDuplicate(arr1));
+});
+console.log(result.join(""));
