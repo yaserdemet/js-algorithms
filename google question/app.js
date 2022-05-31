88505739529c2712f584991382b0be321905b514
yaser <ddemety372@gmail.com>
2022-05-31 14:58:33 +0300

    solution with for of

--- google question/app.js
+++ google question/app.js
@@ -4,21 +4,38 @@
 
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-const arr = [14, 15, 3, 7];
-let x = 17;
+// const arr = [14, 15, 3, 7];
+// let x = 17;
 
-function myFunc(arr , x) {
-    for(let i = 0 ; i < arr.length ; i++) {
-        for(let j = 0; j < arr.length ; j++){
-            if(arr[i] + arr[j] ===x) {
-                return true;
+
+// ! solution with standart for loop
+// function myFunc(arr , x) {
+//     for(let i = 0 ; i < arr.length ; i++) {
+//         for(let j = 0; j < arr.length ; j++){
+//             if(arr[i] + arr[j] ===x) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+// console.log(myFunc(arr, x));
+
+
+
+// ? solution with for of loop
+
+function total (arr,x) {
+    for(let i of arr) {
+        for(let j of arr) {
+            if(i +j === x) {
+                return true
             }
         }
+        return false;
     }
-    return false;
 }
-console.log(myFunc(arr, x));
-
+console.log(total([14, 15, 3, 7], 17));
 
 
 
