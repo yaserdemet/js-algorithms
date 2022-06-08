5189a37aa46225997882cd2f353a164a392eae51
yaser <ddemety372@gmail.com>
2022-06-08 19:15:08 +0300

    solution with filter was added

--- unq/app.js
+++ unq/app.js
@@ -1,16 +1,30 @@
-let z = [ 0, 0, 0.55, 0, 0 ]
-
 // ! find the unique one !
+// ? SOLUTION WITH INDEX OF METHOD;
 
+//  let empty = []
+//  let empty2 = []
+//  function unq(arr) {
+//      for(let i= 0; i < arr.length; i++) {
+//          empty.push(arr[0])
+//          if(empty.indexOf(arr[i]) === -1){
+//              empty2.push(arr[i])
+//          }
+//    }
+//      return empty2 }
+//  console.log(unq([ 0, 0, 0.55, 0, 0 ]));
+
+
+
+// ! SOLUTION WITH FILTER METHOD;
+
+let z = [ 0, 0, 0.55, 0, 0 ]
 let empty = []
-let empty2 = []
-function unq(arr) {
-    for(let i= 0; i < arr.length; i++) {
-        empty.push(arr[0])
-        if(empty.indexOf(arr[i]) === -1){
-            empty2.push(arr[i])
-        }
-    }
-    return empty2
+function unq2(arr){
+   
+   empty.push(arr[0])
+
+ let h=  z.filter(y => !empty.includes(y))
+ return h
+
 }
-console.log(unq([ 0, 0, 0.55, 0, 0 ]));
+console.log(unq2([ 0, 0, 0.55, 0, 0 ]));
